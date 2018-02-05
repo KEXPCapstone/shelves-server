bc31fa496e72bb1306816646726ed92f9ba3c76f
Adam Bourn <abourn@uw.edu>
2018-02-05 14:01:23 -0800

    Add constants, add skeleton main func

---
+++ gateway/handlers/constants.go
@@ -0,0 +1,25 @@
+package handlers
+
+const headerContentType = "Content-Type"
+
+const contentTypeJSON = "application/json"
+
+const contentTypePlainText = "text/plain"
+
+const accessControlAllowOrigin = "Access-Control-Allow-Origin"
+
+const accessControlAllowMethods = "Access-Control-Allow-Methods"
+
+const accessControlAllowHeaders = "Access-Control-Allow-Headers"
+
+const accessControlExposeHeaders = "Access-Control-Expose-Headers"
+
+const accessControlMaxAge = "Access-Control-Max-Age"
+
+const valAllowOrigin = "*"
+
+const valAllowMethods = "GET,PUT,POST,PATCH,DELETE,OPTIONS"
+
+const valAllowHeaders = "Content-Type,Authorization"
+
+const valExposeHeaders = "Authorization"
