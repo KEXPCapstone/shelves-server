c400fa835961378b828d49f0edd772505381c985
Adam Bourn <abourn@uw.edu>
2018-02-12 10:50:33 -0800

    Add resource path comments to LibraryStore interface

--- library/libraryStore.go
+++ library/libraryStore.go
@@ -4,18 +4,22 @@ type LibraryStore interface {
 
 	// TODO: return single track
 	// Track will have associated release ID
+	// "GET /v1/library/tracks/{id}"
 	getTrackById(id int) error
 
 	// TODO: return single release
 	// TODO: decide if release has trackIds or track structs
 	// Release will have associated artist ID
+	// "GET /v1/library/releases/{id}"
 	getReleaseById(id int) error
 
 	// TODO: Evaluate if necessary
 	// Will return associated release
+	// TODO: Resource path
 	getReleaseByTrackId(id int) error
 
 	// TODO: Return a slice of releases within provided category
+	// "GET /v1/library/releases/categories/{cat}"
 	getReleasesByCategory() error
 
 	// TODO: Rename method?
@@ -23,5 +27,6 @@ type LibraryStore interface {
 	// the given releaseID
 	// TODO: "SELECT * FROM Releases WHERE criterion == criterion" with
 	// a switch on criterion
+	// "GET /v1/releases/related/{id}"
 	getRelatedReleasesByReleaseId(id int, criterion string) error
 }
