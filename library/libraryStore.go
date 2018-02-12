2056bb4d10c97a5cd3f58619b78715927b17482e
Adam Bourn <abourn@uw.edu>
2018-02-12 10:32:58 -0800

    Add LibraryStore interface method stubs

--- library/libraryStore.go
+++ library/libraryStore.go
@@ -1,4 +1,27 @@
 package library
 
 type LibraryStore interface {
+
+	// TODO: return single track
+	// Track will have associated release ID
+	getTrackById(id int) error
+
+	// TODO: return single release
+	// TODO: decide if release has trackIds or track structs
+	// Release will have associated artist ID
+	getReleaseById(id int) error
+
+	// TODO: Evaluate if necessary
+	// Will return associated release
+	getReleaseByTrackId(id int) error
+
+	// TODO: Return a slice of releases within provided category
+	getReleasesByCategory() error
+
+	// TODO: Rename method?
+	// TODO: Return a slice of releases which share the criterion with
+	// the given releaseID
+	// TODO: "SELECT * FROM Releases WHERE criterion == criterion" with
+	// a switch on criterion
+	getRelatedReleasesByReleaseId(id int, criterion string) error
 }
