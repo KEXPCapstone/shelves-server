b69cdffcbbed34fe8004bd7b97dc10637cd99fa5
Adam Bourn <abourn@uw.edu>
2018-02-12 10:36:25 -0800

    Add resource path comments to ShelvesStore interface

--- shelves/shelvesStore.go
+++ shelves/shelvesStore.go
@@ -2,18 +2,27 @@ package shelves
 
 // TODO: Evaluate correct type of 'id' parameters
 type ShelvesStore interface {
+
 	// TODO: return array of shelves
+	// "GET /v1/shelves/"
 	getShelves() error
 
 	// TODO: return single shelf
+	// "GET /v1/shelves/{id}"
 	getShelfById(id int) error
 
+	// TODO: Will accept a typeof 'Shelf' and replace exisiting shelf
+	// at id with that new Shelf
+	// "PUT /v1/shelves/{id}"
 	updateShelf(id int) error
 
+	// "DELETE /v1/shelves/{id}"
 	deleteShelf(id int) error
 
-	// TODO: return copy of shelf with given id
+	// TODO: create and return copy of shelf with given id
+	// TODO: Create resource path for this function
 	copyShelf(id int) error
 
+	// TODO: export provided shelf as a folder to Dalet
 	exportShelf(id int) error
 }
