3cfba41d4a97350f5e2d020775923a157db8e236
Adam Bourn <abourn@uw.edu>
2018-02-12 10:26:08 -0800

    Add ShelvesStore interface method stubs

--- shelves/shelvesStore.go
+++ shelves/shelvesStore.go
@@ -1,4 +1,19 @@
 package shelves
 
+// TODO: Evaluate correct type of 'id' parameters
 type ShelvesStore interface {
+	// TODO: return array of shelves
+	getShelves() error
+
+	// TODO: return single shelf
+	getShelfById(id int) error
+
+	updateShelf(id int) error
+
+	deleteShelf(id int) error
+
+	// TODO: return copy of shelf with given id
+	copyShelf(id int) error
+
+	exportShelf(id int) error
 }
