09943334c3c46113efb636d466401e2a930bdd77
Adam Bourn <abourn@uw.edu>
2018-02-14 14:48:01 -0800

    Add user structs, user method stubs

---
+++ gateway/models/users/user.go
@@ -0,0 +1,47 @@
+package users
+
+var bcryptCost = 13
+
+//User represents a user account in the database
+type User struct {
+	// ID        bson.ObjectId `json:"id" bson:"_id"`  // TODO: Change, this is something from mongo
+	Email     string `json:"email"`
+	PassHash  []byte `json:"-"` //stored, but not encoded to clients
+	UserName  string `json:"userName"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+//Credentials represents user sign-in credentials
+type Credentials struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+//NewUser represents a new user signing up for an account
+type NewUser struct {
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	PasswordConf string `json:"passwordConf"`
+	UserName     string `json:"userName"`
+	FirstName    string `json:"firstName"`
+	LastName     string `json:"lastName"`
+}
+
+//Updates represents allowed updates to a user profile
+type Updates struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func (nu *NewUser) Validate() error {}
+
+func (nu *NewUser) ToUser() (*User, error) {}
+
+func (u *User) FullName() string {}
+
+func (u *User) SetPassword(password string) error {}
+
+func (u *User) Authenticate(password string) error {}
+
+func (u *User) ApplyUpdates(updates *Updates) error {}
