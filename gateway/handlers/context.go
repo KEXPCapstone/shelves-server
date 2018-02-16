75a2fae22bda536dd0200644906f21393b52a44c
Adam Bourn <abourn@uw.edu>
2018-02-16 11:06:15 -0800

    Add userStore field to handlerCtx

--- gateway/handlers/context.go
+++ gateway/handlers/context.go
@@ -1,17 +1,23 @@
 package handlers
 
-import "github.com/KEXPCapstone/shelves-server/gateway/sessions"
+import (
+	"github.com/KEXPCapstone/shelves-server/gateway/models/users"
+	"github.com/KEXPCapstone/shelves-server/gateway/sessions"
+)
 
 type HandlerCtx struct {
 	signingKey   string
 	sessionStore sessions.Store
+	userStore    users.UserStore
+
 	// TODO
 }
 
-func NewHandlerContext(signingKey string, ss sessions.Store) *HandlerCtx {
+func NewHandlerContext(signingKey string, ss sessions.Store, us users.UserStore) *HandlerCtx {
 	// TODO
 	return &HandlerCtx{
 		signingKey:   signingKey,
 		sessionStore: ss,
+		userStore:    us,
 	}
 }
