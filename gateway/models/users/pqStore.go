5a3e0cd7f64c85b61bfdf04b4d17592165fb6aec
Adam Bourn <abourn@uw.edu>
2018-02-16 11:42:53 -0800

    Add method stubs to user pqstore

--- gateway/models/users/pqStore.go
+++ gateway/models/users/pqStore.go
@@ -14,3 +14,16 @@ func (ps *PqStore) Insert(nu *NewUser) (*User, error) {
 	// added to a row in the database, and then write the
 	// returned id to the User
 }
+
+func (ps *PqStore) GetByID(id int) (*User, error) {}
+
+//GetByEmail returns the User with the given email
+func (ps *PqStore) GetByEmail(email string) (*User, error) {}
+
+//GetByUserName returns the User with the given Username
+func (ps *PqStore) GetByUserName(username string) (*User, error) {}
+
+func (ps *PqStore) Update(userID int, updates *Updates) error {}
+
+//Delete deletes the user with the given ID
+func (ps *PqStore) Delete(userID int) error {}
