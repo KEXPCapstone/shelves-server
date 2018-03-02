2f13679559d4f64e67f4f23d3cf8d8fed7c533a1
Adam Bourn <abourn@uw.edu>
2018-03-01 21:31:17 -0800

    Add GetUserShelves interface method, edit GetShelves method return types

---
+++ shelves/shelfstore.go
@@ -0,0 +1,33 @@
+package shelves
+
+import "gopkg.in/mgo.v2/bson"
+
+// TODO: Evaluate correct type of 'id' parameters
+type ShelvesStore interface {
+	Insert(ns *NewShelf) (*Shelf, error)
+
+	// TODO: return array of shelves
+	// "GET /v1/shelves/"
+	GetShelves() ([]*Shelf, error)
+
+	GetUserShelves(userId bson.ObjectId) ([]*Shelf, error)
+
+	// TODO: return single shelf
+	// "GET /v1/shelves/{id}"
+	GetShelfById(id bson.ObjectId) (*Shelf, error)
+
+	// TODO: Will accept a typeof 'Shelf' and replace exisiting shelf
+	// at id with that new Shelf
+	// "PUT /v1/shelves/{id}"
+	UpdateShelf(id bson.ObjectId) error
+
+	// "DELETE /v1/shelves/{id}"
+	DeleteShelf(id bson.ObjectId) error
+
+	// TODO: create and return copy of shelf with given id
+	// TODO: Create resource path for this function
+	CopyShelf(id bson.ObjectId, userId bson.ObjectId) (*Shelf, error)
+
+	// TODO: export provided shelf as a folder to Dalet
+	ExportShelf(id bson.ObjectId) error
+}
