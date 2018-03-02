d809974c6488e2a0a539fe8ef74aaffd7f761162
Adam Bourn <abourn@uw.edu>
2018-03-01 20:37:27 -0800

    Edit params of shelvesStore methods for bson ids

--- shelves/shelvesStore.go
+++ shelves/shelvesStore.go
@@ -1,5 +1,7 @@
 package shelves
 
+import "gopkg.in/mgo.v2/bson"
+
 // TODO: Evaluate correct type of 'id' parameters
 type ShelvesStore interface {
 
@@ -9,20 +11,20 @@ type ShelvesStore interface {
 
 	// TODO: return single shelf
 	// "GET /v1/shelves/{id}"
-	GetShelfById(id int) error
+	GetShelfById(id bson.ObjectId) error
 
 	// TODO: Will accept a typeof 'Shelf' and replace exisiting shelf
 	// at id with that new Shelf
 	// "PUT /v1/shelves/{id}"
-	UpdateShelf(id int) error
+	UpdateShelf(id bson.ObjectId) error
 
 	// "DELETE /v1/shelves/{id}"
-	DeleteShelf(id int) error
+	DeleteShelf(id bson.ObjectId) error
 
 	// TODO: create and return copy of shelf with given id
 	// TODO: Create resource path for this function
-	CopyShelf(id int) error
+	CopyShelf(id bson.ObjectId, userId bson.ObjectId) error
 
 	// TODO: export provided shelf as a folder to Dalet
-	ExportShelf(id int) error
+	ExportShelf(id bson.ObjectId) error
 }
