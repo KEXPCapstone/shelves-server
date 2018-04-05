a6978c1bcdb34fa5e4535835ee2927ded8e933e3
Adam Bourn <abourn@uw.edu>
2018-04-05 14:39:30 -0700

    Fix deploy script domain reference; rename Redis container

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ echo "Deploying to DigitalOcean Droplet; You may be prompted to enter your SSH p
 ssh root@api.kexpshelves.com << HERE
 	docker network rm dylan
 	docker network create dylan
-	docker run -d --name redisServer --network dylan redis
+	docker run -d --name redissvr --network dylan redis
 	docker run -d --name mongodb --network dylan mongo
 	docker pull kexpcapstone/shelves
 	docker rm -f shelves
@@ -26,10 +26,10 @@ ssh root@api.kexpshelves.com << HERE
 	-v /etc/letsencrypt:/tls:ro \
 	-e SHELVESSVCADDR=shelves:80 \
 	-e LIBRARYSVCADDR=library:80 \
-	-e TLSCERT=/tls/live/api.kexpcapstone.com/fullchain.pem \
-	-e TLSKEY=/tls/live/api.kexpcapstone.com/privkey.pem \
+	-e TLSCERT=/tls/live/api.kexpshelves.com/fullchain.pem \
+	-e TLSKEY=/tls/live/api.kexpshelves.com/privkey.pem \
 	-e DBADDR=mongodb:27017 \
-	-e REDISADDR=redisServer:6379 \
+	-e REDISADDR=redissvr:6379 \
 	-e SESSIONKEY=blackcoffee \
 	--network dylan \
 	kexpcapstone/gateway
