6af585f03510a5ac06be98504b7c714410f09ee7
Adam Bourn <abourn@uw.edu>
2018-04-06 12:05:51 -0700

    Add DaletRelease struct

--- library/release.go
+++ library/release.go
@@ -21,6 +21,38 @@ type Release struct {
 	// Add image URL?
 }
 
+type DaletRelease struct {
+	KEXPPrimaryGenre              string `json:"KEXPPrimaryGenre"`
+	KEXPMBID                      string `json:"KEXPMBID"`
+	KEXPDateReleased              string `json:"KEXPDateReleased"`
+	KEXPFirstReleaseDate          string `json:"KEXPFirstReleaseDate"`
+	KEXPLength                    string `json:"KEXPLength"`
+	KEXPReleaseCatalogNumber      string `json:"KEXPReleaseCatalogNumber"` // couldn't find
+	KEXPReleaseGroupMBID          string `json:"KEXPReleaseGroupMBID"`
+	KEXPTitle                     string `json:"KEXPTitle"`
+	KEXPUniqueTitle               string `json:"KEXPUniqueTitle"`
+	KEXPReleaseArtistCredit       string `json:"KEXPReleaseArtistCredit"`
+	KEXPArtist                    string `json:"KEXPArtist"`
+	KEXPLabel                     string `json:"KEXPLabel"`
+	KEXPReleasePackaging          string `json:"KEXPReleasePackaging"`
+	KEXPReleasePrimaryType        string `json:"KEXPReleasePrimaryType"`
+	KEXPReleaseSecondaryType      string `json:"KEXPReleaseSecondaryType"`
+	KEXPReleaseStatus             string `json:"KEXPReleaseStatus"`
+	KEXPArtist_KEXPAlias          string `json:"KEXPArtist_KEXPAlias"`
+	KEXPArtist_KEXPArtistType     string `json:"KEXPArtist_KEXPArtistType"`
+	KEXPArtist_KEXPDisambiguation string `json:"KEXPArtist_KEXPDisambiguation"`
+	KEXPArtist_KEXPLink           string `json:"KEXPArtist_KEXPLink"`
+	KEXPArtist_KEXPMBID           string `json:"KEXPArtist_KEXPMBID"`
+	KEXPArtist_KEXPName           string `json:"KEXPArtist_KEXPName"`
+	KEXPArtist_KEXPSortName       string `json:"KEXPArtist_KEXPSortName"`
+	KEXPLabel_KEXPMBID            string `json:"KEXPLabel_KEXPMBID"`
+	KEXPLabel_KEXPName            string `json:"KEXPLabel_KEXPName"`
+	KEXPLabel_KEXPSortName        string `json:"KEXPLabel_KEXPSortName"`
+	KEXPArea                      string `json:"KEXPArea"`
+	KEXPAreaMBID                  string `json:"KEXPAreaMBID"`
+	KEXPCountryCode               string `json:"KEXPCountryCode"`
+}
+
 type MusicBrainzMeta struct {
 	MBID string `json:"mbid"`
 	// TODO: All of the meta that we want.
