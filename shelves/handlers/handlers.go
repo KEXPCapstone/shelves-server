b9e4ad6c2debc817a0b2408b98a1bd6a2e126e4a
Adam Bourn <abourn@uw.edu>
2018-04-19 14:50:13 -0700

    Add placeholder ShelvesHandler and ShelfHandler

--- shelves/handlers/handlers.go
+++ shelves/handlers/handlers.go
@@ -1,1 +1,22 @@
 package handlers
+
+import "net/http"
+
+// /v1/shelves
+func (hCtx *HandlerCtx) ShelvesHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+	default:
+		http.Error(w, "", http.StatusMethodNotAllowed)
+		return
+	}
+}
+
+// /v1/shelves/{id}
+func (hCtx *HandlerCtx) ShelfHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	default:
+		http.Error(w, "", http.StatusMethodNotAllowed)
+		return
+	}
+}
