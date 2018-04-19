f8e841925923797f3df1fb256a0227d4e46d244f
Adam Bourn <abourn@uw.edu>
2018-04-19 16:47:40 -0700

    Add respond.go constants in shelves

--- shelves/handlers/constants.go
+++ shelves/handlers/constants.go
@@ -5,3 +5,9 @@ import "errors"
 const XUser = "X-User"
 
 const ErrInvalidShelfID = errors.New("Invalid Shelf ID")
+
+const headerContentType = "Content-Type"
+
+const contentTypeJSON = "application/json"
+
+const ErrEncodingJSON = "Error converting response value to JSON: "
