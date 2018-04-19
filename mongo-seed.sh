b7eef164e1d705f7327fe2cf87982901e9694731
John Laws Harrison <johnlawsharrison@gmail.com>
2018-04-19 11:17:04 -0700

    Create autoimport script for mongo image

---
+++ mongo-seed.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "running mongoimport"
+mongoimport --host mongodb --db library --collection releases --type json -v --file /data/seeds/dalet_deploy.json --jsonArray
+echo "creating artists collection from release data"
+mongo mongodb/library --eval 'db.releases.aggregate([{$group: {_id: "$KEXPReleaseArtistCredit", releases: {$push: {id: "$_id", KEXPTitle: "$KEXPTitle", KEXPMBID: "$KEXPMBID"}}}},{$sort:{_id:1}},{$out: "artists"}],{collation:{locale: "en", strength: 1}})'
