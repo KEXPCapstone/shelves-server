838e8ce150ea42157a4b9ae89da0e627d6777402
Adam Bourn <abourn@uw.edu>
2018-04-23 21:07:52 -0700

    Register FeaturedShelvesHandler to mux; closes #73

--- shelves/main.go
+++ shelves/main.go
@@ -45,6 +45,7 @@ func main() {
 	mux.HandleFunc("/v1/shelves", hCtx.ShelvesHandler)
 	mux.HandleFunc("/v1/shelves/mine", hCtx.ShelvesMineHandler)
 	mux.HandleFunc("/v1/shelves/users/", hCtx.UserShelvesHandler)
+	mux.HandleFunc("/v1/shelves/featured", hCtx.FeaturedShelvesHandler)
 	mux.HandleFunc("/v1/shelves/", hCtx.ShelfHandler)
 
 	log.Printf("The 'shelves' microservice is listening at http://%s...", addr)
