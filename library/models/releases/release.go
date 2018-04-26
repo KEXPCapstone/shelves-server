54dbf98ab4bb168bfd42c46626a01c13481e5ade
Adam Bourn <abourn@uw.edu>
2018-04-26 15:30:00 -0700

    Add notes field to Release struct

--- library/models/releases/release.go
+++ library/models/releases/release.go
@@ -12,25 +12,26 @@ import (
 // with a combination of fields from KEXP's own records, MetaData from MusicBrainz
 // and custom fields related to the 'shelves' application
 type Release struct {
-	ID                      string        `json:"id" bson:"_id"`
-	ArtistCredit            []interface{} `json:"artistCredit" bson:"artist-credit"`
-	ReleaseEvents           []interface{} `json:"releaseEvents" bson:"release-events"`
-	CoverArtArchive         interface{}   `json:"coverArtArchive" bson:"cover-art-archive"`
-	KEXPReleaseGroupMBID    string        `json:"KEXPReleaseGroupMBID" bson:"KEXPReleaseGroupMBID"`
-	KEXPReleaseArtistCredit string        `json:"KEXPReleaseArtistCredit" bson:"KEXPReleaseArtistCredit"`
-	LabelInfo               []interface{} `json:"labelInfo" bson:"label-info"`
-	Media                   []interface{} `json:"media" bson:"media"`
-	Status                  string        `json:"status" bson:"status"`
-	Disambiguation          string        `json:"disambiguation" bson:"disambiguation"`
-	Barcode                 string        `json:"barcode" bson:"barcode"`
-	Packaging               string        `json:"packaging" bson:"packaging"`
-	Date                    string        `json:"date" bson:"date"`
-	ASIN                    string        `json:"asin" bson:"asin"`
-	KEXPPrimaryGenre        string        `json:"KEXPPrimaryGenre" bson:"KEXPPrimaryGenre"`
-	Title                   string        `json:"title" bson:"title"`
-	CountryCode             string        `json:"countryCode" bson:"country"`
-	Yellows                 []int         `json:"yellows" bson:"yellows"`
-	Reds                    []int         `json:"reds" bson:"reds"`
+	ID                      string          `json:"id" bson:"_id"`
+	ArtistCredit            []interface{}   `json:"artistCredit" bson:"artist-credit"`
+	ReleaseEvents           []interface{}   `json:"releaseEvents" bson:"release-events"`
+	CoverArtArchive         interface{}     `json:"coverArtArchive" bson:"cover-art-archive"`
+	KEXPReleaseGroupMBID    string          `json:"KEXPReleaseGroupMBID" bson:"KEXPReleaseGroupMBID"`
+	KEXPReleaseArtistCredit string          `json:"KEXPReleaseArtistCredit" bson:"KEXPReleaseArtistCredit"`
+	LabelInfo               []interface{}   `json:"labelInfo" bson:"label-info"`
+	Media                   []interface{}   `json:"media" bson:"media"`
+	Status                  string          `json:"status" bson:"status"`
+	Disambiguation          string          `json:"disambiguation" bson:"disambiguation"`
+	Barcode                 string          `json:"barcode" bson:"barcode"`
+	Packaging               string          `json:"packaging" bson:"packaging"`
+	Date                    string          `json:"date" bson:"date"`
+	ASIN                    string          `json:"asin" bson:"asin"`
+	KEXPPrimaryGenre        string          `json:"KEXPPrimaryGenre" bson:"KEXPPrimaryGenre"`
+	Title                   string          `json:"title" bson:"title"`
+	CountryCode             string          `json:"countryCode" bson:"country"`
+	Yellows                 []int           `json:"yellows" bson:"yellows"`
+	Reds                    []int           `json:"reds" bson:"reds"`
+	Notes                   []bson.ObjectId `json:"noteIDs" bson:"noteIDs"`
 }
 
 // ReleaseAndMatchCriteria represents a pairing of
