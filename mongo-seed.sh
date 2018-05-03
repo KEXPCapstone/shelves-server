c60ee60d5e93a44fc6e3ea186171392a259de45e
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-02 19:29:24 -0700

    Redefine artists aggregate seed

--- mongo-seed.sh
+++ mongo-seed.sh
@@ -4,4 +4,49 @@
 echo "running mongoimport"
 mongoimport --host mongodb --db library --collection releases --type json -v --file /data/seeds/dalet_deploy.json --jsonArray
 echo "creating artists collection from release data"
-mongo mongodb/library --eval 'db.releases.aggregate([{$group: {_id: "$KEXPReleaseArtistCredit", releases: {$push: {id: "$_id", KEXPTitle: "$KEXPTitle", KEXPMBID: "$KEXPMBID"}}}},{$sort:{_id:1}},{$out: "artists"}],{collation:{locale: "en", strength: 1}})'
+mongo mongodb/library --eval 'db.releases.aggregate([{$group: {_id: "$KEXPReleaseGroupMBID",artist:{$first:{$arrayElemAt: ["$artist-credit", 0]}},releases: {$push: {id: "$_id"}}}},{$group: {_id: "$artist.artist.id",artistName: {$first: "$artist.artist.name"},artistSortName: {$first: "$artist.artist.sort-name"},disambiguation: {$first: "$artist.artist.disambiguation"},releaseGroups:{$push:{id: "$_id",releases: "$releases"}}}},{$sort:{_id:1}},{$out: "artists"}],{collation:{locale: "en", strength: 1}})'
+
+# mongo aggregate
+# (same as above, but for readability)
+# db.releases.aggregate([
+#     {
+#         $group: {
+#             _id: "$KEXPReleaseGroupMBID",
+#             artist: {
+#                 $first: {
+#                     $arrayElemAt: ["$artist-credit", 0]
+#                 }
+#             },
+#             releases: {
+#                 $push: {
+#                     id: "$_id"
+#                 }
+#             }
+#         }
+#     },
+#     {
+#         $group: {
+#             _id: "$artist.artist.id",
+#             artistName: {
+#                 $first: "$artist.artist.name"
+#             },
+#             artistSortName: {
+#                 $first: "$artist.artist.sort-name"
+#             },
+#             disambiguation: {
+#                 $first: "$artist.artist.disambiguation"
+#             },
+#             releaseGroups: {
+#                 $push: {
+#                     id: "$_id",
+#                     releases: "$releases"
+#                 }
+#             }
+#         }
+#     },
+#     {
+#         $sort:{_id:1}
+#     },
+#     {
+#         $out: "artists"
+#     }],{collation:{locale: "en", strength: 1}})
