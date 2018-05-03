7ab3179a72dd926fc89ae05c6acdc3021f2aeb9d
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-02 19:41:12 -0700

    Update artist model

--- library/models/releases/artist.go
+++ library/models/releases/artist.go
@@ -3,14 +3,8 @@ package releases
 // Artist represents an artist in the KEXP library, with additional
 // information about associated releases
 type Artist struct {
-	ArtistMBID string           `json:"artistMBID" bson:"_id"`
-	Name       string           `json:"name" bson:"name"`
-	Releases   []ReleaseSummary `json:"releases" bson:"releases"`
-}
-
-// ReleaseSummary represents a summary of a given release with minimal metadata
-type ReleaseSummary struct {
-	ReleaseMBID string `json:"releaseMBID" bson:"ReleaseMBID"`
-	Title       string `json:"title" bson:"title"`
-	// ... more fields to be added as needed
+	ArtistMBID     string        `json:"artistMBID" bson:"_id"`
+	ArtistName     string        `json:"name" bson:"artistName"`
+	ArtistSortName string        `json:"artistSortName" bson:"artistSortName"`
+	ReleaseGroups  []interface{} `json:"releaseGroups" bson:"releaseGroups"`
 }
