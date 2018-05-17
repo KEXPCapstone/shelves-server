62a78c91c8b836e1114addb6bed88825f078031a
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-16 18:23:04 -0700

    Update json fieldnames for groupings

--- library/models/releases/artist.go
+++ library/models/releases/artist.go
@@ -3,9 +3,9 @@ package releases
 // Artist represents an artist in the KEXP library, with additional
 // information about associated releases
 type Artist struct {
-	ArtistMBID     string        `json:"artistMBID" bson:"_id"`
+	ArtistMBID     string        `json:"id" bson:"_id"`
 	ArtistName     string        `json:"name" bson:"artistName"`
-	ArtistSortName string        `json:"artistSortName" bson:"artistSortName"`
+	ArtistSortName string        `json:"sortName" bson:"artistSortName"`
 	Disambiguation string        `json:"disambiguation" bson:"disambiguation"`
 	ReleaseGroups  []interface{} `json:"releaseGroups" bson:"releaseGroups"`
 }
