f0c337dac763ac2b946fb1b0bcd2373b23bec012
John Laws Harrison <johnlawsharrison@gmail.com>
2018-05-23 13:28:50 -0700

    Add label, status, date to artist releases

--- mongo-seed.sh
+++ mongo-seed.sh
@@ -4,7 +4,7 @@
 echo "running mongoimport"
 mongoimport --host mongodb --db library --collection releases --type json -v --file /data/seeds/dalet_deploy.json --jsonArray
 echo "creating artists collection from release data"
-mongo mongodb/library --eval 'db.releases.aggregate([{$group:{_id:"$KEXPReleaseGroupMBID",artist:{$first:{$arrayElemAt:["$artist-credit",0]}},title:{$first:"$KEXPTitle"},releases:{$push:{id:"$_id",title:"$KEXPTitle",coverArtArchive: "$cover-art-archive",asin: "$asin"}}}},{$group:{_id:"$artist.artist.id",artistName:{$first:"$artist.artist.name"},artistSortName:{$first:"$artist.artist.sort-name"},disambiguation:{$first:"$artist.artist.disambiguation"},releaseGroups:{$push:{releaseGroupId:"$_id",title:"$title",releases:"$releases"}}}},{$sort:{_id:1}},{$out:"artists"}],{allowDiskUse: true,collation:{locale:"en",strength:1}})'
+mongo mongodb/library --eval 'db.releases.aggregate([{$group:{_id:"$KEXPReleaseGroupMBID",artist:{$first:{$arrayElemAt:["$artist-credit",0]}},title:{$first:"$KEXPTitle"},releases:{$push:{id:"$_id",title:"$KEXPTitle",coverArtArchive: "$cover-art-archive",asin: "$asin", date: "$date", status: "$status", labelInfo: "$label-info"}}}},{$group:{_id:"$artist.artist.id",artistName:{$first:"$artist.artist.name"},artistSortName:{$first:"$artist.artist.sort-name"},disambiguation:{$first:"$artist.artist.disambiguation"},releaseGroups:{$push:{releaseGroupId:"$_id",title:"$title",releases:"$releases"}}}},{$sort:{_id:1}},{$out:"artists"}],{allowDiskUse: true,collation:{locale:"en",strength:1}})'
 echo "creating labels collection from release data"
 mongo mongodb/library --eval 'db.releases.aggregate([{$addFields:{label:{$arrayElemAt:["$label-info",0]},artist:{$arrayElemAt:["$artist-credit",0]}}},{$group:{_id:"$label.label.id",labelName:{$first:"$label.label.name"},disambiguation:{$first:"$label.label.disambiguation"},releases:{$push:{releaseID:"$_id",releaseGroupID:"$KEXPReleaseGroupMBID",title:"$title",artistName:"$artist.name",artistID:"$artist.artist.id",catalogNumber:"$label.catalog-number",asin: "$asin", coverArtArchive: "$cover-art-archive"}}},},{$out:"labels"}], {allowDiskUse: true})'
 
@@ -27,7 +27,10 @@ mongo mongodb/library --eval 'db.releases.aggregate([{$addFields:{label:{$arrayE
 #                     id: "$_id",
 #                     title: "$KEXPTitle",
 #                     coverArtArchive: "$cover-art-archive",
-#                     asin: "$asin"
+#                     asin: "$asin",
+#                     date: "$date",
+#                     status: "$status",
+#                     labelInfo: "$label-info"
 #                 }
 #             }
 #         }
